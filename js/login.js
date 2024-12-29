2a01bf07aaaea017b02463676bd880b0af116384
SYL748 <lsun22245@gmail.com>
2024-12-28 22:17:36 -0500

    dsf

---
+++ js/login.js
@@ -0,0 +1,18 @@
+document.getElementById("login-button").addEventListener("click", function (event) {
+    const userName = document.getElementById("userName").value;
+    const pwd = document.getElementById("pwd").value;
+
+    if (userName === "yuri" && pwd === "meow") {
+        event.preventDefault();
+        const form = document.querySelector("form");
+        form.style.transition = "opacity 0.5s ease";
+        form.style.opacity = "0";
+        const wrapper = document.querySelector(".wrapper");
+        wrapper.classList.add("form-success");
+        setTimeout(() => {
+            location.href = "./html/testing.html";
+        }, 2000);
+    } else {
+        alert("你不是兔子本人！！！");
+    }
+});
